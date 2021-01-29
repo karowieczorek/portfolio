a826e18830c79229f7138c3e61b9b4378580af8c
Karolina Wieczorek <karo@Karolinas-MacBook-Pro-2.local>
2021-01-29 21:04:48 +0100

    Add Gulp and Nunjucks

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const nunjucksRender = require('gulp-nunjucks-render');
+
+gulp.task('html', function() {
+    return gulp.src('src/pages/*').pipe(nunjucksRender({
+        path: ['src/templates'],
+    })
+    )
+    .pipe(gulp.dest('site'));
+});
+
+   gulp.task('styles', function() {
+    return gulp.src('src/styles/*.css').pipe(nunjucksRender({
+        path: ['src/templates'],
+        ext: ".css",
+    })
+    )
+    .pipe(gulp.dest('site/styles/'));
+});
+
+gulp.task('default', gulp.parallel('html', 'styles'));
